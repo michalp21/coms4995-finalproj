9825fa5389a5f4efac2ee0a91498125c1a699be2
Daniel Echlin <djechlin@gmail.com>
2018-04-03 00:45:47 -0400

    oppo

--- opponent_action.py
+++ opponent_action.py
@@ -30,6 +30,16 @@ def opponent_action(
 	alternative_values = [(gifts[oi] + blueprint_values[oi])
 		for oi in oppo_infosets]
 
+	## this is all the SECOND thing that happens after the
+	## initial chance node deals out cards
+	aug_get_active_player = 3 - subgame.get_active_player()
+	aug_get_available_actions = lambda: ['T', 'S']
+	aug_get_active_player_infoset = lambda: subgame.get_opponent
+	aug_get_opponent_infoset = lambda: []
+	act_T = lambda: terminal_with(value)
+	act_S = lambda: subgame
+
+
 	augmented = construct_subgame(subgame, action)
 	solve_augmented(augmented, alternative_values,
 		oppo_reach_probailities)
@@ -38,6 +48,19 @@ def opponent_action(
 		gifts[oi] += v_bp[oi] - v_bp(oi.move(a))
 
 
-# run kuhn poker
+def terminal_with(value):
+	get_active_player = lambda: None
+	get_available_actions = lambda: []
+	get_active_player_infoset = lambda: None
+	get_opponent_infoset = lambda: None
+	is_terminal = lambda: True
+	utility = lambda: value
 
-opponent_action([])
+	return {
+		get_active_player: get_active_player,
+		get_available_actions: get_available_actions,
+		get_active_player_infoset: get_active_player_infoset,
+		get_opponent_infoset: get_opponent_infoset,
+		is_terminal: is_terminal,
+		utility: utility
+	}
