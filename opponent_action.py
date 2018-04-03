17d24bc11e2e6971ac257c583abc66ba01df3429
Daniel Echlin <djechlin@gmail.com>
2018-04-02 21:31:28 -0400

    more work

--- opponent_action.py
+++ opponent_action.py
@@ -21,36 +21,22 @@ def opponent_action(
 	subgame,
 	action,
 	gifts,
-	 v_bp,
-	 oppo_reach_probabilities):
+	blueprint_values,
+	oppo_reach_probabilities):
 	gifts = gifts if gifts is not None else defaultdict(int)
 
 	oppo_infosets = set([h.get_opponent_infoset() for h in subgame])
 
-	v_alt = [(gifts[oi] + v_bp[oi]) for oi in oppo_infosets]
+	alternative_values = [(gifts[oi] + blueprint_values[oi])
+		for oi in oppo_infosets]
 
 	augmented = construct_subgame(subgame, action)
-	solve_augmented(augmented, v_alt, oppo_reach_prob_values)
+	solve_augmented(augmented, alternative_values,
+		oppo_reach_probailities)
 
 	for each oi in oppo_infosets:
 		gifts[oi] += v_bp[oi] - v_bp(oi.move(a))
 
-# Literally, the pseudocode
-'''
-def function opponent_action(S, a, g):
-	for each node h in S_top:
-		pi_oppo[history] = pi_oppo(history) # copy
-	for each oppo_infoset in S_top:
-		c = oppo_infoset.oppo_cards()
-		v_bp[oppo_infoset] = v_bp(oppo_infoset) # copy
-		v_alt[oppo_infoset] = g[c] + v_bp[oppo_infoset]
-	S_aug = construct_subgame(S, a)
-	solve_augmented(S_aug, v_alt, pi_oppo)
-	for each oppo_infoset in S_top:
-		c = oppo_infoset.oppo_cards()
-		g_new[c] = g[c] + v_bp[oppo_infoset] - v_bp[oppo_infoset, a]
-	return g_new
-'''
 
 # run kuhn poker
 
