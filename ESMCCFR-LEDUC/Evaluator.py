1324c8bca057c4a82c49b2c7b09d17ad6a204352
edunn39 <ed2729@columbia.edu>
2018-04-24 23:40:29 -0400

    game statistics, test file

--- ESMCCFR-LEDUC/Evaluator.py
+++ ESMCCFR-LEDUC/Evaluator.py
@@ -6,28 +6,20 @@ hunl_evaluator = Evaluator()
 def _leduc_rank(card, flop_card):
 	card_rank = Card.int_to_str(card)[0]
 	flop_rank = Card.int_to_str(flop_card)[0]
+	return _leduc_rank_str(card_rank, flop_rank)
+
+def _leduc_rank_str(card_rank, flop_rank):
 	if card_rank == flop_rank:
-		if card_rank == 'A':
-			return 1
-		elif card_rank == 'K':
-			return 2
-		elif card_rank == 'Q':
-			return 3
-		else:
-			raise Exception('How did we get here?')
+		return {'A':1, 'K':2, 'Q':3}[card_rank]
 	else:
-		if card_rank == 'A':
-			return 4
-		elif card_rank == 'K':
-			return 5
-		elif card_rank == 'Q':
-			return 6
-		else:
-			raise Exception('How did we get here?')
+		return {'A':4, 'K':5, 'Q':6}[card_rank]
 
 def leduc_evaluate(p1_hole, p2_hole, board):
 	# return positive if p1 wins
 	return _leduc_rank(p2_hole[0], board[0][0]) - _leduc_rank(p1_hole[0], board[0][0])
+
+def leduc_evaluate_str(p1_hole, p2_hole, board):
+	return _leduc_rank_str(p2_hole[0], board[0]) - _leduc_rank_str(p1_hole[0], board[0])
 
 def kuhn_evaluate(p1_hole, p2_hole, board):
 	# no board, one card hole, bigger card wins
@@ -36,3 +28,4 @@ def kuhn_evaluate(p1_hole, p2_hole, board):
 def hunl_evaluate(p1_hole, p2_hole, board):
 	board = board[0] + board[1] + board[2]
 	return hunl_evaluator.evaluate(p2_hole, board) - hunl_evaluator.evaluate(p1_hole, board)
+
