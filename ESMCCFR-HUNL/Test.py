dc351ec294bea95d2bbdd7c1d61d438eb4ca2125
mp3242 <mp3242@columbia.edu>
2018-05-01 23:09:24 -0400

    Remove outdated code

--- ESMCCFR-HUNL/Test.py
+++
@@ -1,3 +0,0 @@
-from deuces2.card import Card
-
-print (Card.int_to_str(1))
--- ESMCCFR/ActionAndAmount.py
+++
@@ -1,9 +0,0 @@
-# An ActionAndAmount consists of an action = fold, check, call, bet, raise and an amount
-class ActionAndAmount:
-
-	def __init__(self, action, amount):
-		self.action = action
-		self.amount = amount
-
-	def __eq__(self, other):
-		return self.action == other.action and self.amount == other.amount
