2c978b950abc76b009f602aa206e92fd0b993bcd
Daniel Echlin <djechlin@gmail.com>
2018-05-13 20:05:01 -0400

    read/write rows separate

--- ESMCCFR-LEDUC/StrategySaver.py
+++ ESMCCFR-LEDUC/StrategySaver.py
@@ -4,15 +4,12 @@ import csv
 
 def save(filename, infoset_strategy_map):
 	with open(filename, 'w') as f:
-		writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_NONE)
-		infosets = [(k, v.get_average_strategy(), v.count, v.regret_sum) for k,v in infoset_strategy_map.items()]
-		for count, i in enumerate(infosets):
-			infoset = i[0]
-			writer.writerow([infoset.hole,
-				infoset.board,
-				_comma_join(infoset.bets_0),
-				_comma_join(infoset.bets_1),
-				_comma_join(['%d' % (1000 * round(k, 3)) for k in i[1]])])
+
+		writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
+		for infoset, v in enumerate(self.infoset_strategy_map.items(0)):
+			row = _write_row(infoset, v)
+			assert _read_row(row)[0] == infoset
+			writer.writerow(row)
 			if count % 10000 == 0:
 				print("Flushing %d / %d" % (count, len(infosets)))
 				f.flush()
@@ -24,24 +21,35 @@ def load(filename):
 		reader = csv.reader(f, delimiter=';')
 		i = 1
 		for row in reader:
-			hole = row[0]
-			board = row[1]
-			bets_0 = _comma_split_int(row[2])
-			bets_1 = _comma_split_int(row[3])
+			infoset, strategy = _read_row(row)
+			infoset_strategy_map[infoset] = strategy
+			print(" ", i, end='\r')
+			i += 1
 
-			infoset = InfoSet(hole, board, (bets_0, bets_1))
+	return infoset_strategy_map
 
-			strategy = Strategy(0)
-			strategy.average_strategy = [float(x)/1000 for x in _comma_split_int(row[4])]
+def _write_row(infoset, v):
+	return [infoset.hole,
+			infoset.board,
+			_comma_join(infoset.bets_0),
+			_comma_join(infoset.bets_1),
+			_comma_join('%d' % (1000 * round(k, 3)) for k in v)]
+
+def _read_row(row):
+	hole = row[0]
+	board = row[1]
+	bets_0 = _comma_split_int(row[2])
+	bets_1 =_comma_split_int(row[3])
+
+	infoset = InfoSet(hole, board, (bets_0, bets_1))
+
+	strategy = Strategy(0)
+	strategy.average_strategy = [float(x)/1000 for x in _comma_split_int(row[4])]
 			sum_strategy = sum(strategy.average_strategy)
 			for s in strategy.average_strategy:
 				s /= sum_strategy
 
-			infoset_strategy_map[infoset] = strategy
-			print(" ", i, end='\r')
-			i += 1
-	return infoset_strategy_map
-
+	return infoset, strategy
 
 
 def _comma_join(arr):
